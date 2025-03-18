57132c9ff395535e8d4d65751f1f91bf1acfd261
Chineze Okadigbo <72756219+neze14@users.noreply.github.com>
2025-03-18 20:26:20 +0100

    add swagger to main

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,8 @@ import { ConfigModule } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 import { RolesGuard } from './auth/guards/roles.guard';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 
@@ -18,6 +20,17 @@ async function bootstrap() {
   // Apply RolesGuard globally
   // const reflector = app.get(Reflector);
   // app.useGlobalGuards(new RolesGuard(reflector));
+
+
+  // Swagger setup
+  const config = new DocumentBuilder()
+    .setTitle('API Documentation')
+    .setDescription('API description for the application')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
 
   app.setGlobalPrefix('api/v1');
   app.enableCors({ origin: 'http://localhost:8080' });
